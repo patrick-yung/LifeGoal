03a847a34bc0c8c4690ba0ba4819233dcfbe5440
Patrick Yung <patrickyyung@gmail.com>
2026-05-27 03:26:59 -0400

    Init Commit - - - ___________ 27/05/2026

---
+++ app/src/main/java/com/cencol/patrick/lifegoals_patrickyung/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.cencol.patrick.lifegoals_patrickyung
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.cencol.patrick.lifegoals_patrickyung.ui.theme.LifeGoals_PatrickYungTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LifeGoals_PatrickYungTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LifeGoals_PatrickYungTheme {
+        Greeting("Android")
+    }
+}
